3665d3ab7d27664d151d7b912b5b0088d2722777
Opxzy22 <113675564+Opxzy22@users.noreply.github.com>
2024-01-16 13:34:13 +0100

    a measure time function

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+"""
+    define a measure_time function
+"""
 
 import time
 import asyncio
@@ -20,6 +23,6 @@ def measure_time(n: int, max_delay: int) -> float:
     start_time = time.perf_counter()
     asyncio.run(wait_n(n, max_delay))
     end_time = time.perf_counter()
-    total_time = start_time - end_time
+    total_time = end_time - start_time
 
     return total_time / n
